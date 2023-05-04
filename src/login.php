9cf8ac70ff57eafed238d1e38f0a8d5d5acf2edf
MagickFreak <matteo.marvell@gmail.com>
2023-05-04 21:24:20 +0200

    md5 password

--- src/login.php
+++ src/login.php
@@ -1,20 +1,23 @@
 <?php
     session_start();
     require './db_connect/connect.php';
+    require 'function.php';
 
     $email=$_POST['email'];
     $password=$_POST['password'];
+    $password=sanitizeString($password);
 
     $sql= "SELECT email FROM user WHERE $email = 'email'";
     $result=$conn->query($sql);
     if($result->num_rows=0){
         echo "Email  inesistenti";
     }else{
+        $password=md5($password);
         $sql="SELECT * FROM user WHERE $email = 'email' AND $password = 'password'";
         $result=$conn->query($sql);
         if($result->num_rows>0){
             session_start();
-            $_SESSION['user']=$result->fetch_assoc()['id'];
+            $_SESSION['id']=$row['id'];
             header('Location: home.php');
         }
         echo "Login errato";
--- src/register.php
+++ src/register.php
@@ -1,16 +1,19 @@
 <?php
     require '../config/connection.php';
+    require 'function.php';
 
     $firstname=$_POST["firstname"];
     $lastname=$_POST["lastname"];
     $email=$_POST["email"];
     $password=$_POST["password"];
+    $password=sanitizeString($password);
 
     $sql="SELECT * FROM user WHERE email='$email'";
     $result=$conn->query($sql);
     if($result->num_rows>0){
         echo "L'utente con questa email: ".$email." è già registrato";
     }else{
+        $password=md5($password);
         $sql="INSERT INTO user(firstname,lastname,email,password) VALUES('$firstname','$lastname','$email','$password')";
         if($conn->query($sql)===TRUE){
             header('Location: login.html');
